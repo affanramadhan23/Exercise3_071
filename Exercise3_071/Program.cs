655971171659ecfee4b4dc8038872ea5af718700
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-25 16:17:55 +0700

    add class node and Circular List with a function public bool search

--- Exercise3_071/Program.cs
+++ Exercise3_071/Program.cs
@@ -4,6 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace Exercise_Linked_List_D
+{
+    class node
+    {
+        /*Creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+    class CircularList
+    {
+        node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref node previous, ref node current)
+        /*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = previous.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); //returns true if the nodes is found
+            }
+            if (rollNo == LAST.rollNumber) //if the node is present at the end
+                return (true);
+            else
+                return (false); //returns false if the node is not found 
+        }
+    }
+}
 namespace Exercise3_071
 {
     internal class Program
